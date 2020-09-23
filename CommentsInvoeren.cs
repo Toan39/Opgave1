22e06fbfff33d8276b043910f3d2b7eb7b4e1553
Nothoniel <71545333+Nothoniel@users.noreply.github.com>
2020-09-23 14:59:19 +0200

    Update CommentsInvoeren.cs

--- CommentsInvoeren.cs
+++ CommentsInvoeren.cs
@@ -5,7 +5,7 @@ using System.Globalization;
 
 class OpgaveForm : Form
 {
-
+    //Declaratie van alle Interface-onderdelen die buiten OpgavenForm() aangeroepen moeten kunnen worden
     TextBox PaneelX;
     TextBox PaneelY;
     TextBox PaneelS;
@@ -14,12 +14,18 @@ class OpgaveForm : Form
     Button knopOK;
     Panel Plaatje;
 
+
     public OpgaveForm()
     {
+        //Zorgt ervoor dat een ingevoerde "." wordt geregristreed als een komma, hierdoor gebruiken de invoer en de code allebei een "."
         CultureInfo.CurrentCulture = new CultureInfo("en-US", false);
+
+        //Eigenschappen van de Window zelf
         //Control//
         this.Size = new Size(800, 500);
         this.BackColor = Color.GhostWhite;
+
+        //Maakt voor alle in te voegen variabelen een soort invoerpaneel aan in de vorm van TextBoxes
 
         //PaneelX//
         PaneelX = new TextBox();
@@ -35,56 +41,58 @@ class OpgaveForm : Form
         PaneelY.Location = new Point(650, 10);
         this.Controls.Add(PaneelY);
 
-        //Schaal//
+        //PaneelSchaal//
         PaneelS = new TextBox();
         PaneelS.Text = "0.01";
         PaneelS.Size = new Size(100, 20);
         PaneelS.Location = new Point(500, 70);
         this.Controls.Add(PaneelS);
 
-        //Max//
+        //PaneelMax//
         PaneelM = new TextBox();
         PaneelM.Text = "100";
         PaneelM.Size = new Size(100, 20);
         PaneelM.Location = new Point(650, 70);
         this.Controls.Add(PaneelM);
 
-        //Label for Mid X coordinate//
+        //Om duidelijk te maken waar de TextBoxes voor staan, worden er de volgende Labels aan toegevoegd:
+
+        //Label voor MiddenX//
         Label label1;
         label1 = new Label();
-        label1.Text = "MidX:";
+        label1.Text = "Midden X:";
         label1.Location = new Point(450, 10);
         this.Controls.Add(label1);
 
-        //Label for Mid Y coordinate//
+        //Label voor MiddenY//
         Label label2;
         label2 = new Label();
-        label2.Text = "MidY:";
+        label2.Text = "Midden Y:";
         label2.Location = new Point(600, 10);
         this.Controls.Add(label2);
 
-        //Label for Max value//
+        //Label voor Max//
         Label label3;
         label3 = new Label();
         label3.Text = "Max:";
         label3.Location = new Point(600, 70);
         this.Controls.Add(label3);
 
-        //Label for Scale value//
+        //Label voor de Schaal//
         Label label4;
         label4 = new Label();
         label4.Text = "Schaal:";
         label4.Location = new Point(450, 70);
         this.Controls.Add(label4);
 
-        //Label for voorbeeldenmenu//
+        //Label voor de lijst met voorbeelden//
         Label label5;
         label5 = new Label();
         label5.Text = "Voorbeelden:";
         label5.Location = new Point(550, 200);
         this.Controls.Add(label5);
 
-        //Listbox//
+        //Lijst van de gegeven voorbeelden//
         LijstVB = new ListBox();
         LijstVB.Location = new Point(650, 200);
         LijstVB.Size = new Size(100, 60);
@@ -97,8 +105,8 @@ class OpgaveForm : Form
         this.Controls.Add(LijstVB);
         LijstVB.SelectedIndexChanged += new EventHandler(VoorbeeldenMenu);
 
-
-        //Button okay//
+        //Bij het indrukken van de OK-knop worden de ingevoerde variabelen bevestigd en wordt een nieuw plaatje getekent
+        //OK-knop//
         knopOK = new Button();
         knopOK.Text = "OK";
         knopOK.Size = new Size(50, 20);
