cca71154889824546e9c9a449f8f7411185a1b85
Nothoniel <71545333+Nothoniel@users.noreply.github.com>
2020-10-17 11:46:24 +0200

    Update reversi.cs

--- reversi.cs
+++ reversi.cs
@@ -11,44 +11,53 @@ namespace Reversi
 {
     class Reversi : Form
     {
-        Font ComicSans = new Font("Comic Sans MS", 14);
-        const int lengte = 6, breedte = 6;
-        const int diameter = 29;
-        const int steenoffset = (34 - diameter) / 2;
+        //declaraties
+        Font ComicSans = new Font("Comic Sans MS", 14);//Het font dat wordt gebruikt bij het scorebord
+        const int lengte = 6, breedte = 6;//Het aantal vakjes van het speelbord in de lengte en breedte
+        const int diameter = 29;//Diameter van de stenen
+        const int steenoffset = (34 - diameter) / 2;//Hoeveel de steen vanaf de rand van een vakje moet worden verschoven om in het midden te komen
         bool geeindigd = false, hulpNodig = false;
-        int[,] kl;
-        bool[] insarray;
-        bool[,] leg;
-        private int kleur = 1;
+        int[,] kl;//2D array die opslaat welke kleur elk vakje heeft: 0 voor leeg, 1 voor een blauwe steen en 2 voor een rode
+        bool[,] leg;//Slaat op of een vakje legaal is
+        bool[] insarray;//Slaat voor elke richting op of er stenen ingesloten kunnen worden ingesloten
+        private int kleur = 1;//Kleur die aan de beurt is: 1 is blauw, 2 = rood. Blauw begint.
         private int x;
-        private int y;
+        private int y;//x en y van het geklikte vakje
         private int mogzet = 0, overgeslagen = 0;
         private int aantalrood = 2, aantalblauw = 2;
+
+        //onderdelen v/d interface die ook na de initialisatie worden aangeroepen
         Panel Speelbord;
         Panel Scorebord;
         Label Status;
         CheckBox Helplock;
+
         public Reversi()
         {
+            //Properties van de window
             this.Size = new Size(300, 380);
-            this.kl = new int[breedte, lengte];
-            this.leg = new bool[breedte, lengte];
-            this.insarray = new bool[8];
-            this.x = 0;
-            this.y = 0;
             this.Text = "Reversi";
             this.Paint += tekenscherm;
 
+            //Domeinen van de arrays
+            this.kl = new int[breedte, lengte];
+            this.leg = new bool[breedte, lengte];
+            this.insarray = new bool[8];
+
+            //alle onderdelen van het scherm
+
+            //Speelbord//
             Speelbord = new Panel()
             {
                 Location = new Point(20, 20),
-                Size = new Size(1 + breedte * 33, 1 + lengte * 33),
+                Size = new Size(breedte * 33, lengte * 33),
                 BackColor = Color.Transparent
             };
             Speelbord.Paint += teken;
             Speelbord.MouseClick += klik;
             this.Controls.Add(Speelbord);
 
+            //Score bord met aantal stenen per kleur//
             Scorebord = new Panel()
             {
                 Location = new Point(20, 270),
@@ -57,6 +66,7 @@ namespace Reversi
             Scorebord.Paint += score;
             this.Controls.Add(Scorebord);
 
+            //nieuw spel-knop//
             Button NieuwKnop = new Button()
             {
                 Size = new Size(80, 20),
@@ -66,6 +76,7 @@ namespace Reversi
             NieuwKnop.Click += NieuwSpel;
             this.Controls.Add(NieuwKnop);
 
+            //Hulpknop//
             Button HulpKnop = new Button()
             {
                 Size = new Size(80, 20),
@@ -75,6 +86,7 @@ namespace Reversi
             HulpKnop.Click += Hulp;
             this.Controls.Add(HulpKnop);
 
+            //Chekbox waardoor de "hulpcirkels" na het eindigen van een beurt blijven//
             Helplock = new CheckBox()
             {
                 Location = new Point(200, 254),
@@ -82,6 +94,7 @@ namespace Reversi
             };
             this.Controls.Add(Helplock);
 
+            //Label naast de checkbox//
             Label Lock = new Label()
             {
                 Text = "Lock Help",
@@ -90,6 +103,7 @@ namespace Reversi
             };
             this.Controls.Add(Lock);
 
+            //Status//
             Status = new Label()
             {
                 Size = new Size(150, 20),
@@ -98,73 +112,72 @@ namespace Reversi
             };
             this.Controls.Add(Status);
 
+            //De 4 stenen in het midden
             kl[breedte / 2, lengte / 2] = 1;
             kl[breedte / 2 - 1, lengte / 2] = 2;
             kl[breedte / 2, lengte / 2 - 1] = 2;
             kl[breedte / 2 - 1, lengte / 2 - 1] = 1;
 
-            legaal(kleur);
+            legaal(kleur); //wordt aan het begin van het spel uitgevoerd, zodat de speler op de eerste legale vakjes kan klikken
         }
 
-        public void klik(object sender, MouseEventArgs mea)
+        public void klik(object sender, MouseEventArgs mea)//De eventhandler voor wanneer je op het speelbord klikt
         {
-            x = mea.X / 33;
+            x = mea.X / 33; //met het x- en y-coordinaat wordt berekent op welk vakje de speler heeft geklikt, elk vakje is 33*33 pixels
             y = mea.Y / 33;
 
             if (!geeindigd)
             {
-                if (leg[x, y])
+                if (leg[x, y])//kijkt of het geklikte vakje legaal is
                 {
-                    overgeslagen = 0;
-                    if(!Helplock.Checked) hulpNodig = false;
-                    this.veranderkleur(x, y);
-                    this.kleur = 3 - kleur;
+                    overgeslagen = 0;//aantal overgeslagen beurten in een rij wordt gereset
+                    if(!Helplock.Checked) hulpNodig = false;//Als de hulp niet gelockd is, wordt de hulpfunctie uitgezet 
+                    this.veranderkleur(x, y);//Update het bord
+                    this.kleur = 3 - kleur;//Verandert de kleur die aan de beurt is 3 - 2(rood) = 1(blauw) en vice versa
                 }
-                mogzet = 0;
-                legaal(kleur);
-                while (mogzet == 0 && !geeindigd)
+                mogzet = 0;//Reset de mogelijke zetten voor de volgende beurt
+                legaal(kleur);//wordt uigevoerd (zie de methode)
+                while (mogzet == 0 && !geeindigd)//zolang het aantal mogelijke zetten 0 is(wanneer er een beurt moet worden overgeslagen) en het spel niet is geeindigd
                 {
                     overgeslagen++;
                     if (overgeslagen == 2)
                     {
-                        EindeSpel();
+                        EindeSpel();//als er 2 beurten achter elkaar zijn overgeslagen is het spel afgelopen, het spel kan namelijk niet verder als niemand een zet kan doen
                     }
+                    this.kleur = 3 - kleur;//volgende speler aan de beurt
                     legaal(kleur);
-                    this.kleur = 3 - kleur;
                 }
             }
-            if (!geeindigd)
+            if (!geeindigd) //Deze if-statement zorgt ervoor dat de uitslag van het potje niet wordt overschreven met wie er aan zet is
                 switch (kleur)
                 {
                     case 1: Status.Text = "Blauw is aan zet"; break;
                     case 2: Status.Text = "Rood is aan zet"; break;
-                }
+                } //Laat zien wie er aan zet is
         }
 
-        public void legaal(int kleur)
+        public void legaal(int kleur) //Gaat elk vakje op het bord af en checkt of er een legale zet gedaan kan worden.
         {
             for (int t = 0; t < breedte; t++)
             {
                 for (int u = 0; u < lengte; u++)
                 {
-                    leg[t, u] = richtingen(t, u);
-                    if (leg[t, u]) mogzet++;
+                    leg[t, u] = richtingen(t, u);//"Legale vakjes" worden opgeslagen in een 2-dimensionale array.
+                    if (leg[t, u]) mogzet++;//voor elke mogelijke zet wordt het aantal mogelijke zetten geincrement 
                 }
             }
         }
 
-        public void arrayOpslag(int x, int y)
+        public void arrayOpslag(int x, int y) //Voert insluiten() uit in de 8 nodige richtingen en slaat deze waarden op in een bool array. 
         {
-            this.insarray[0] = insluiten(x, y, 0, -1);
-            this.insarray[1] = insluiten(x, y, -1, -1);
-            this.insarray[2] = insluiten(x, y, -1, 0);
-            this.insarray[3] = insluiten(x, y, -1, 1);
-            this.insarray[4] = insluiten(x, y, 0, 1);
-            this.insarray[5] = insluiten(x, y, 1, 1);
-            this.insarray[6] = insluiten(x, y, 1, 0);
-            this.insarray[7] = insluiten(x, y, 1, -1);
-        }
-
+            this.insarray[0] = insluiten(x, y, 0, -1);//naar boven,
+            this.insarray[1] = insluiten(x, y, -1, -1);//linksboven,
+            this.insarray[2] = insluiten(x, y, -1, 0);//links,
+            this.insarray[3] = insluiten(x, y, -1, 1);//linksonder,
+            this.insarray[4] = insluiten(x, y, 0, 1);//onder,
+            this.insarray[5] = insluiten(x, y, 1, 1);//rechtsonder,
+            this.insarray[6] = insluiten(x, y, 1, 0);//rechts
+            this.insarray[7] = insluiten(x, y, 1, -1);//en rechtsboven.
         // Methode die bepaald welke richtingen ingeklikt kunnen worden
           bool richtingen(int x, int y)
         {
