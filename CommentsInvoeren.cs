6e02a5de4dd8f4e66360c7f87c909119a020d84f
Toan39 <71543094+Toan39@users.noreply.github.com>
2020-11-09 13:23:00 +0100

    Update CommentsInvoeren.cs

--- CommentsInvoeren.cs
+++ CommentsInvoeren.cs
@@ -5,7 +5,7 @@ using System.Globalization;
 
 class OpgaveForm : Form
 {
-    //Declaratie van alle Interface-onderdelen die buiten OpgavenForm() aangeroepen moeten kunnen worden //hoi1234
+    //Declaratie van alle Interface-onderdelen die buiten OpgavenForm() aangeroepen moeten kunnen worden //hoi12345
     TextBox PaneelX;
     TextBox PaneelY;
     TextBox PaneelS;
